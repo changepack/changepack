cf50f8925faa7f5e5a67bc1ad3542bed4e756e50
Kamil Nicieja <kamil@nicieja.co>
2022-04-01 22:33:30 +0200

    CSS

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{erb,haml,html,slim}'
+    './app/components/**/*.{erb,haml,html,slim,rb}'
   ],
   theme: {
     extend: {
