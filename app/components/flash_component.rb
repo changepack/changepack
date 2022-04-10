acaf7d5bff68836067681bb4f152ab8d309d780e
Kamil Nicieja <kamil@nicieja.co>
2022-04-10 18:50:48 +0200

    Add flash messages

---
+++ app/components/flash_component.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class FlashComponent < ApplicationComponent
+  option :type
+
+  def call
+    content_tag(:div) do
+      content_tag(:p, class: "py-2 px-3 mb-10 font-medium rounded-lg inline-block #{color}") do
+        content
+      end
+    end
+  end
+
+  private
+
+  def color
+    case type.to_sym
+    when :notice
+      'bg-green-50 text-green-500'
+    when :alert
+      'bg-yellow-50 text-yellow-500'
+    end
+  end
+
+  def render?
+    content.present?
+  end
+end
