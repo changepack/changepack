09696f50d8a4f74f3d4fe8263111ed2c66165603
Kamil Nicieja <kamil@nicieja.co>
2022-04-10 21:35:26 +0200

    Use native fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
       },
       colors: {
         orange: {
