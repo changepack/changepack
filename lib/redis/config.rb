63cce9ccd82d21540d8ef3c0fc1e3aaff98a1355
Kamil Nicieja <kamil@nicieja.co>
2022-09-14 00:09:49 +0200

    Update Sidekiq

--- lib/redis/config.rb
+++ lib/redis/config.rb
@@ -1,11 +1,9 @@
 class Redis
   def self.configuration
     @configuration ||= {
-      url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
+      url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
       password: ENV.fetch('REDIS_PASSWORD', nil).presence,
-      ssl_params: { verify_mode: Changepack.redis_ssl_verify_mode },
-      reconnect_attempts: 2,
-      network_timeout: 5
+      ssl_params: { verify_mode: Changepack.redis_ssl_verify_mode }
     }
   end
 end
