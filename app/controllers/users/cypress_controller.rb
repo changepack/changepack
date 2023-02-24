6aab6c9379ab790c0e95c891a1dc43050f5ab4ec
Kamil Nicieja <kamil@nicieja.co>
2023-02-24 02:19:55 +0100

    Protect Users::CypressController

--- app/controllers/users/cypress_controller.rb
+++ app/controllers/users/cypress_controller.rb
@@ -5,6 +5,8 @@ module Users
     skip_before_action :verify_authenticity_token, :authenticate_user!
 
     def authenticate
+      return unless Rails.env.test?
+
       sign_in(user)
       redirect_to URI.parse(params.require(:redirect_to)).path
     end
