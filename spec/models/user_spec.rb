a0d574f473ae415fbc68bd7ecfc8b87e4eee2bd4
Kamil Nicieja <kamil@nicieja.co>
2023-03-11 12:39:05 +0100

    Unify form styles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 describe User do
-  it { is_expected.to validate_presence_of(:name) }
-
   it { is_expected.to belong_to(:account) }
   it { is_expected.to have_many(:changelogs).dependent(:nullify) }
 end
