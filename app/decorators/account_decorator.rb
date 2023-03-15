bb45b9e7c964065ef882b3cb0048a389225e8057
Kamil Nicieja <kamil@nicieja.co>
2023-03-15 02:17:31 +0100

    Add AccountPictureComponent

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 class AccountDecorator < ApplicationDecorator
-  DEFAULT_PICTURE = 'M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z' # rubocop:disable Layout/LineLength
   SIZE = [48, 48].freeze
 
-  def display_picture
+  def thumb
     return picture if picture.blank?
 
-    @display_picture ||= picture.variant(resize_to_fill: SIZE)
-  end
-
-  def picture_tag
-    if picture.present?
-      helpers.image_tag(display_picture)
-    else
-      helpers.content_tag(:svg, class: 'h-full w-full text-gray-300', fill: 'currentColor', viewBox: '0 0 24 24') do
-        helpers.content_tag(:path, nil, d: DEFAULT_PICTURE)
-      end
-    end
+    @thumb ||= picture.variant(resize_to_fill: SIZE)
   end
 end
