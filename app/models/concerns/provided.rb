cb52c9da977b7c5f7c68de56d1ff743b57634663
Kamil Nicieja <kamil@nicieja.co>
2023-05-05 23:10:05 +0200

    Add signatures to Provided

--- app/models/concerns/provided.rb
+++ app/models/concerns/provided.rb
@@ -3,6 +3,7 @@
 
 module Provided
   extend ActiveSupport::Concern
+  extend T::Sig
 
   class Provided < Event
     attribute :id, String
@@ -16,17 +17,24 @@ module Provided
   end
 
   class_methods do
+    extend T::Sig
+
+    sig { params(name: T::Key).returns(T::Boolean) }
     def provider(name)
       define_method(name) { providers[name.to_s] }
       define_singleton_method(name) { where('providers -> ? IS NOT NULL', name) }
       define_method("#{name}?") { providers[name.to_s].present? }
+
+      true
     end
   end
 
+  sig { returns String }
   def provider
     providers.keys.first
   end
 
+  sig { returns String }
   def provided!
     pub Provided.new(id:, providers:)
   end
