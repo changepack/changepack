eb8a56294f50adbcb20f4f2198653d33980b722c
Kamil Nicieja <kamil@nicieja.co>
2023-05-06 19:23:17 +0200

    Add comments that explain why updates and sources reference models from other domains

--- write/lib/source.rb
+++ write/lib/source.rb
@@ -14,14 +14,22 @@ class Source < ApplicationRecord
 
   key :src
 
+  # Refer to the following comment. We only duplicate attributes essential for
+  # filters, reports, or SQL queries, such as those used to scope paginated
+  # repositories and teams together, for instance.
   attribute :name, :string
   attribute :type, :string
   attribute :status, :string, default: :inactive
 
   belongs_to :account
   belongs_to :changelog, optional: true
+  # We are violating the guideline of avoiding cross-domain model references here,
+  # as sources, repositories, and teams typically change in tandem. Our goal is to
+  # prevent the added complexity of caching other attributes from repositories
+  # and teams, which you can reference directly.
   belongs_to :repository, optional: true
   belongs_to :team, optional: true
+
   has_many :updates, dependent: :destroy
 
   validates :name, presence: true
--- write/lib/update.rb
+++ write/lib/update.rb
@@ -27,6 +27,9 @@ class Update < ApplicationRecord
 
   key :upd
 
+  # Refer to the following comment. We only duplicate attributes essential for
+  # filters, reports, or SQL queries, such as those used to scope paginated
+  # commits and issues together, for instance.
   attribute :name, :string
   attribute :type, :string
   attribute :email, :string
@@ -35,6 +38,10 @@ class Update < ApplicationRecord
   belongs_to :source
   belongs_to :post, optional: true
   belongs_to :changelog, optional: true
+  # We are violating the guideline of avoiding cross-domain model references here,
+  # as updates, commits, and issues typically change in tandem. Our goal is to
+  # prevent the added complexity of caching other attributes from commits and
+  # issues, which you can reference directly.
   belongs_to :commit, optional: true
   belongs_to :issue, optional: true
 
