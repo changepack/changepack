8b9ce9459e416fbe6d9c7725434b0ce518b93581
Kamil Nicieja <kamil@nicieja.co>
2023-06-08 22:57:36 +0200

    Derive published from published_at

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,24 +9,32 @@ module API
           required(:content).filled(:string)
 
           optional(:title).filled(:string)
-          optional(:published).filled(:bool)
-          optional(:published_at).filled(:time)
+          optional(:published_at).filled(:date)
         end
       end
 
       def create
-        publication = Publication.new(
-          post: Post.new,
-          account: current_bearer,
-          **validation_result.to_h.except(:published_at)
-        ).tap(&:create)
-
+        publication.create
         publication.post.update!(published_at:) if published_at.present?
 
         render json: publication.post, status: :created
       end
 
       private
+
+      def publication
+        @publication ||= Publication.new(
+          post: Post.new,
+          account: current_bearer,
+          **attributes
+        )
+      end
+
+      def attributes
+        validation_result.to_h
+                         .except(:published_at)
+                         .merge(published: published_at.present?)
+      end
 
       def published_at
         validation_result[:published_at]
