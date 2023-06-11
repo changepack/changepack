b81c2204e2bc75e51c89d2a099689742a7784b5f
Kamil Nicieja <kamil@nicieja.co>
2023-06-12 00:34:23 +0200

    Test ChangelogPolicy

---
+++ spec/policies/changelog_policy_spec.rb
@@ -0,0 +1,20 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ChangelogPolicy, type: :policy do
+  let(:user) { build(:user, account: record) }
+  let(:record) { build(:account) }
+
+  let(:context) { { user: } }
+
+  let(:policy) { described_class.new(record, **context) }
+
+  describe_rule :index? do
+    succeed 'with a user'
+    failed 'without a user' do
+      let(:user) { nil }
+    end
+  end
+end
