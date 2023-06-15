1e4420a3b174d5df03c597293b4dbbfed3680b16
Kamil Nicieja <kamil@nicieja.co>
2023-06-16 01:43:34 +0200

    Simplify the test

--- spec/controllers/sources_controller_spec.rb
+++ spec/controllers/sources_controller_spec.rb
@@ -2,24 +2,16 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
 RSpec.describe SourcesController do
   include Devise::Test::ControllerHelpers
-RSpec.describe SourcesController do
+
+  before { sign_in create(:user) }
+
   describe 'GET #index' do
     it 'authorizes the user' do
       get :index
       expect(response).to have_http_status(:success)
-    end
-
-    it 'paginates the sources' do
-      10.times { Source.create }
-      get :index, params: { page: 2 }
-      expect(assigns(:sources).length).to eq(5)
-    end
-
-    it 'renders the sources' do
-      get :index
-      expect(response).to render_template(:index)
     end
   end
 end
