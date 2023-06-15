3353a94ee60a33249fa4b779f6928093d871d95c
Kamil Nicieja <kamil@nicieja.co>
2023-06-16 01:33:56 +0200

    Fix tests

--- spec/controllers/sources_controller_spec.rb
+++ spec/controllers/sources_controller_spec.rb
@@ -1,38 +1,24 @@
-spec/controllers/sources_controller_spec.rb
-</new_file>
+# typed: false
+# frozen_string_literal: true
 
-```ruby
 require 'rails_helper'
 
-RSpec.describe SourcesController, type: :controller do
+RSpec.describe SourcesController do
   describe 'GET #index' do
     it 'authorizes the user' do
-get :index
-expect(response).to have_http_status(:success)
-      # Add test here
+      get :index
+      expect(response).to have_http_status(:success)
     end
 
     it 'paginates the sources' do
-10.times { Source.create }
-get :index, params: { page: 2 }
-expect(assigns(:sources).length).to eq(5)
-      # Add test here
+      10.times { Source.create }
+      get :index, params: { page: 2 }
+      expect(assigns(:sources).length).to eq(5)
     end
 
     it 'renders the sources' do
-get :index
-expect(response).to render_template(:index)
-      # Add test here
-    end
-  end
-
-  describe '#collection' do
-    it 'fetches the authorized sources that are kept and have activity' do
-Source.create(active: true)
-get :collection
-expect(assigns(:sources).length).to eq(1)
-      # Add test here
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 end
-```
