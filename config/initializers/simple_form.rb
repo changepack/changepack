db18a9fab0ee93dc9cf46ff76380daf593840cf9
Kamil Nicieja <kamil@nicieja.co>
2023-06-19 22:01:36 +0200

    Mark optional fields instead of required ones

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,7 +5,37 @@
 # See https://github.com/heartcombo/simple_form#custom-components to know
 # more about custom components.
 # Dir[Rails.root.join('lib/components/**/*.rb')].each { |f| require f }
-#
+module SimpleForm
+  module Components
+    module LabelNecessity
+      module ClassMethods #:nodoc:
+        def translate_optional_html
+          I18n.t(:"simple_form.optional.html", default:
+            %[<abbr title="#{translate_optional_text}">#{translate_optional_mark}</abbr>]
+          )
+        end
+
+        def translate_optional_text
+          I18n.t(:"simple_form.optional.text", default: 'optional')
+        end
+
+        def translate_optional_mark
+          I18n.t(:"simple_form.optional.mark", default: '*')
+        end
+      end
+
+      protected
+
+      def required_label_text #:nodoc:
+        required_field? ? self.class.translate_required_html.dup : self.class.translate_optional_html.dup
+      end
+    end
+  end
+end
+
+SimpleForm::Components::Labels.prepend SimpleForm::Components::LabelNecessity
+SimpleForm::Components::Labels::ClassMethods.include SimpleForm::Components::LabelNecessity::ClassMethods
+
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   # Wrappers are used by the form builder to generate a
